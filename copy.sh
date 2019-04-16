e73b74f32c4a17d51a14cae74df650cc10b22570
dbierer <doug@unlikelysource.com>
2019-04-16 14:23:19 +0000

    2019-04-18 DB: updated for mod_alias and mod_rewrite labs

--- copy.sh
+++ copy.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/sh
 
-# Copies files from this repo to their proper place in the VM
-cp -r ./var/www/* /var/www
-cp -r ./home/vagrant/* /home/vagrant
+# Assumes $1 is the apache group ("daemon" if installed from source)
+GROUP=$1
+if [ -z "$1" ]
+  then
+    GROUP="daemon"
+fi
 
+# Copies files from this repo to their proper place in the VM
+cp -ruv ./var/www/* /var/www
+chown -R vagrant:${GROUP} /var/www
+chmod -R 775 /var/www
+cp -ruv ./home/vagrant/* /home/vagrant
+chown -R vagrant:${GROUP} /home/vagrant
+chmod -R 775 /home/vagrant
